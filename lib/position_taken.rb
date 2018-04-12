928c5f892ba38fe170810d20ed2405bc378c53fd
tylerlove <tml.335@gmail.com>
2018-04-12 20:51:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == " " or board[index] == ""
+  if board[index] == " " or board[index] == "" or board[index] == nil
     return false
   end
 end
